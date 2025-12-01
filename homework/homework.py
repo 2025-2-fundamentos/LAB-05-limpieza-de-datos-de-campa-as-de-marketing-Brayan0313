4148d160a4069e367ee0e3db2337304d9d44a296
Brayan0313 <69543843+Brayan0313@users.noreply.github.com>
2025-12-01 10:06:59 -0500

    Intento

--- homework/homework.py
+++ homework/homework.py
@@ -5,6 +5,67 @@ Escriba el codigo que ejecute la accion solicitada.
 # pylint: disable=import-outside-toplevel
 
 
+"""
+Escriba el codigo que ejecute la accion solicitada.
+"""
+import pandas as pd
+import zipfile
+from pathlib import Path
+import os
+import glob
+# pylint: disable=import-outside-toplevel
+
+def load_input(input):
+
+    dataframes = []
+
+    input_folder = Path(input)
+
+    for archivo in input_folder.iterdir():
+
+        with zipfile.ZipFile(archivo) as zip_file:
+                for csv_file in zip_file.namelist():
+                    with zip_file.open(csv_file) as file:
+                        df = pd.read_csv(file)
+                        dataframes.append(df)
+    
+    dataframe = pd.concat(dataframes, ignore_index=True)
+
+    return dataframe
+
+def save_output(dataframe1, dataframe2, dataframe3, output_directory, output_name1, output_name2, output_name3):
+    """Save output to a file."""
+
+    if os.path.exists(output_directory):
+        files = glob.glob(f"{output_directory}/*")
+        for file in files:
+            os.remove(file)
+        os.rmdir(output_directory)
+
+    os.makedirs(output_directory)
+
+    dataframe1.to_csv(
+        f"{output_directory}/{output_name1}",
+        sep=",",
+        index=False,
+        header=True,
+    )
+    dataframe2.to_csv(
+        f"{output_directory}/{output_name2}",
+        sep=",",
+        index=False,
+        header=True,
+    )
+
+    dataframe3.to_csv(
+        f"{output_directory}/{output_name3}",
+        sep=",",
+        index=False,
+        header=True,
+    )
+
+
+
 def clean_campaign_data():
     """
     En esta tarea se le pide que limpie los datos de una campaña de
@@ -49,8 +110,57 @@ def clean_campaign_data():
 
 
     """
+    df = load_input('files/input')
+    #cleaning data from df
 
-    return
+    #cambios en la columna 'job'
+    df['job'] = df['job'].str.replace('.','', regex=False).str.replace('-','_', regex=False)
+
+    #cambios en la columna education
+    df['education'] = df['education'].str.replace('.','_', regex=False)
+    df['education'] = df['education'].replace('unknown',pd.NA)
+
+    #cambios en la columna credit_default
+    df['credit_default'] = df['credit_default'].replace('yes',1, regex=False)
+    df.loc[df['credit_default'] != 1, 'credit_default'] = 0
+
+    #cambios en la columna mortgage
+    df['mortgage'] = df['mortgage'].replace('yes',1, regex=False)
+    df.loc[df['mortgage'] != 1, 'mortgage'] = 0
+
+    #cambios en previous_outcome
+    df['previous_outcome'] = df['previous_outcome'].replace('success',1)
+    df.loc[df['previous_outcome'] != 1, 'previous_outcome'] = 0
+
+    #cambios en campaign_outcome
+    df['campaign_outcome'] = df['campaign_outcome'].replace('yes',1)
+    df.loc[df['campaign_outcome'] != 1, 'campaign_outcome'] = 0
+
+    #creacion de last_contact_day
+    meses_a_numeros = {
+    'mar': '03',
+    'apr': '04',
+    'may': '05',
+    'jun': '06',
+    'jul': '07',
+    'aug': '08',
+    'sep': '09',
+    'oct': '10',
+    'nov': '11',
+    'dec': '12'
+    }
+
+    df['month'] = df['month'].map(meses_a_numeros)
+    df['last_contact_date'] = '2022' + '-' + df['month'] + '-' + df['day'].astype(str)
+
+    #crear los 3 df
+    client_df = df.iloc[:, 1:8 ]
+
+    campaign_df = df.iloc[:, [1,10,11,12,13,16,17]]
+
+    economics_df = df.iloc[:, [1,14,15]]
+
+    save_output(client_df, campaign_df, economics_df, 'files/output', 'client.csv', 'campaign.csv', 'economics.csv')
 
 
 if __name__ == "__main__":
